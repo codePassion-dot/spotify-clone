bd54519fa43a27dc44d3991c7449b0f39a30fa97
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-02-05 17:18:12 -0500

    Added custom checkbox
    - The checkbox of the spotify login is by far no simple
    - added custom css code to make the done arrow visible when the checkbox
      state is checked
    - Added checkbox react componet that handle the whole input

---
+++ src/components/CheckBox.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+class CheckBox extends Component {
+  render() {
+    const { doneArrow } = this.props;
+    return (
+      <label htmlFor="check-box-recover" className="relative cursor-pointer ">
+        <input
+          className=" mr-2 h-2 w-2 appearance-none rounded-md border border-gray-400 p-2 checked:bg-checkbox-green "
+          type="checkbox"
+          id="check-box-recover"
+          name="recover"
+        />
+        <img
+          className="check-arrow invisible absolute inset-0 h-4 w-4"
+          src={doneArrow}
+          alt="done arrow"
+        />
+        Remeber me
+      </label>
+    );
+  }
+}
+
+CheckBox.propTypes = {
+  doneArrow: PropTypes.string.isRequired,
+};
+
+export default CheckBox;
