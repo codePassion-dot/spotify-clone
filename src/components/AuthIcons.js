a5726f0be6fade9eac97d82b29db239b325dcba4
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-02-05 17:14:15 -0500

    Added needed images for auth page
    - due to the quantity of the images, a specific javascript object was
      created to manage them

---
+++ src/components/AuthIcons.js
@@ -0,0 +1,15 @@
+import facebook from "#/facebook.svg";
+import google from "#/google.svg";
+import phone from "#/phone.svg";
+import apple from "#/apple.svg";
+import spotify from "#/spotify-logo.png";
+import doneArrow from "#/done-arrow.svg";
+
+export const AuthIcons = {
+  facebook,
+  google,
+  phone,
+  apple,
+  spotify,
+  doneArrow,
+};
