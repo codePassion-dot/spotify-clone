5c4ea08df32136c0bdb0781cbdd9ef95ad1e01f3
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-02-05 09:41:39 -0500

    Updated blueprint to use all the dev dependecies

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "plugin:import/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
