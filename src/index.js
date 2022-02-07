cdf9556b60a5a3b005a03c434fb39e35d35cf132
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-02-06 19:30:21 -0500

    Addded router-routes to the entry point

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import NavBar from "./components/navbar/NavBar";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="home" element={<NavBar />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
